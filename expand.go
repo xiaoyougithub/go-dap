8b6ef823e9270904eea19b14c7c39dffc18aeed7
hongcha <zhangsifan98@gmail.com>
2022-09-16 12:27:11 +0800

    custom extension

--- expand.go
+++ expand.go
@@ -1,1 +1,19 @@
 package dap
+
+type SetBreakpointsListEvent struct {
+	Event
+	Arguments []SetBreakpointsArguments `json:"arguments"`
+}
+
+func (r *SetBreakpointsListEvent) GetEvent() *Event { return &r.Event }
+
+type StoppedDataEvent struct {
+	Event
+	Body             StoppedEventBody              `json:"body"`
+	ThreadsBody      ThreadsResponseBody           `json:"threads_body"`
+	StackTraceBody   StackTraceResponseBody        `json:"stack_trace_body"`
+	ScopesBody       ScopesResponseBody            `json:"scopes_body"`
+	VariablesBodyMap map[int]VariablesResponseBody `json:"variables_body_map"`
+}
+
+func (r *StoppedDataEvent) GetEvent() *Event { return &r.Event }
